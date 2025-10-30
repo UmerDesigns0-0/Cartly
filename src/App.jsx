d32d406d48eaebe79cfe5b49852ded3de0432275
UmerDesigns0-0 <umerandlife@gmail.com>
2025-10-30 16:28:23 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -104,7 +104,7 @@ function App() {
                     }
                   />
                   <Route
-                    path="/CheckOut"
+                    path="/Checkout"
                     element={
                       <main>
                         <Checkout />
